1afc088efb5cd847ec6d9176c2ffd9a6a2c0dc61
Kacprodyl <kacprodyl@gmail.com>
2023-03-28 21:02:43 +0200

    create DbCon and DGV classes, modify adding books, add books deleting etc

---
+++ library/library/DGV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace library
+{
+    public class DGV
+    {
+        public static void RefreshDGV(DataGridView dgv, string tableName)
+            // Refresh DGV by using dgv name and name of the Table in DB
+        {
+            dgv.DataSource = null;
+            dgv.Rows.Clear();
+            var dataAdapter = new SqlDataAdapter($"SELECT * FROM {tableName}", DbCon.ConnectionString);
+            var dataSet = new DataSet();
+            dataAdapter.Fill(dataSet, tableName);
+            dgv.DataSource = dataSet.Tables[tableName];
+        }
+    }
+}
